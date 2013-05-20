ad2c2ce9a36cd184dceaeb3da9a86c1b738fc682
ryanluoo <quick.sword@hotmail.com>
2013-05-21 00:30:16 +0800

    doing Riding the Fences

--- USACO 3.3/USACO_Riding the Fences/USACO_Riding the Fences/fence_main.cpp
+++ USACO 3.3/USACO_Riding the Fences/USACO_Riding the Fences/fence_main.cpp
@@ -6,13 +6,25 @@ LANG: C++
 
 #include <fstream>
 #include <cstring>
+#include <map>
 using namespace std;
 
+#define MAXI 500
+int fence[MAXI+1][MAXI+1] = {0};
 int main()
 {
 	ifstream fin ("fence.in" );
 	ofstream fout("fence.out");
 
+	int F;
+	fin >> F;
+	for (int i = 0; i != F; ++i)
+	{
+		int a, b;
+		fin >> a >> b;
+		fence[a][b] = 1;
+		fence[b][a] = 1;
+	}
 	
 	fin.close();
 	fout.close();
