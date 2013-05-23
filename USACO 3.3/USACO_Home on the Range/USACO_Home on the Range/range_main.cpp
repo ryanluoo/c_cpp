63bec5cd845a159ae2728d35d216da8ce74140f3
ryanluoo <quick.sword@hotmail.com>
2013-05-23 19:49:32 +0800

    finish "Home On the Range"

--- USACO 3.3/USACO_Home on the Range/USACO_Home on the Range/range_main.cpp
+++ USACO 3.3/USACO_Home on the Range/USACO_Home on the Range/range_main.cpp
@@ -8,12 +8,57 @@ LANG: C++
 #include <cstring>
 using namespace std;
 
+#define MAXN 250
+int N;
+char field[MAXN][MAXN+1];
+char count[MAXN][MAXN];
 int main()
 {
 	ifstream fin ("range.in" );
 	ofstream fout("range.out");
 
+	fin >> N;
+	fin.getline(field[0], MAXN+1);
+	for (int i = 0; i != N; ++i)
+	{
+		fin.getline(field[i], N+1);
+		for (int j = 0; j != N; ++j)
+		{
+			field[i][j] -= '0';
+			count[i][j] = field[i][j];
+		}
+	}
 	
+	for (int i = 2; i <= N; ++i)
+	{
+		int num = 0;
+		for (int a = 0; a <= N - i; ++a)
+		{
+			for (int b = 0; b <= N - i; ++b)
+			{
+				bool flag = true;
+				if (!count[a][b])
+					continue;
+				for (int j = 0; j != i; ++j)
+				{
+					if (!field[a+i-1][b+j] || !field[a+j][b+i-1])
+					{
+						flag = false;
+						break;
+					}
+				}
+				if (flag)
+				{
+					++num;
+					count[a][b] = 1;
+				}
+				else
+					count[a][b] = 0;
+			}
+		}
+		if (num != 0)
+			fout << i << " " << num << endl;
+	}
 	fin.close();
 	fout.close();
 	return 0;
