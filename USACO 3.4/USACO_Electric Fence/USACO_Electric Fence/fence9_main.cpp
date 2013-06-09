878632eaed0f0a8f3c076116cfafb7ae06b78aab
ryanluoo <quick.sword@hotmail.com>
2013-06-09 14:00:22 +0800

    finish Electric Fence

--- USACO 3.4/USACO_Electric Fence/USACO_Electric Fence/fence9_main.cpp
+++ USACO 3.4/USACO_Electric Fence/USACO_Electric Fence/fence9_main.cpp
@@ -8,10 +8,56 @@ LANG: C++
 #include <cstring>
 using namespace std;
 
+#define MAXNM 32000
+
+double N, M, P;
 int main()
 {
 	ifstream fin ("fence9.in" );
 	ofstream fout("fence9.out");
+	fin >> N >> M >> P;
+
+	int omn = 0;
+	int pONl = 0;
+	for (int i = 1; i < N; ++i)
+	{
+		double y = M / N * i;
+		int yi = y;
+		if (y == (double)yi)
+			++pONl;
+	}
+
+	omn = N == 0 ? 0 : ((N - 1) * (M - 1) - pONl) / 2;
+
+	pONl = 0;
+	int pmn = 0;
+	if (P == N)
+		fout << omn << endl;
+	else if (P > N)
+	{
+		for (int i = N + 1; i < P; ++i)
+		{
+			double y = (P - i) * M / (P - N);
+			int yi = y;
+			if (y == (double)yi)
+				++pONl;
+		}
+		pmn = ((P - N - 1) * (M - 1) - pONl) / 2;
+		int c = N == 0 ? 0 : M - 1;
+		fout << pmn + omn + c << endl;
+	}
+	else
+	{
+		for (int i = P + 1; i < N; ++i)
+		{
+			double y = (i - P) * M / (N - P);
+			int yi = y;
+			if (y == (double)yi)
+				++pONl;
+		}
+		pmn = ((N - P - 1) * (M - 1) - pONl) / 2;
+		fout << omn - pONl - pmn << endl;
+	}
 
 	fin.close();
 	fout.close();
