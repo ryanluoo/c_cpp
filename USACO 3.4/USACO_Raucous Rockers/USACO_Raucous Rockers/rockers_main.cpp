0e2c8e380398c5d6daab052ef0d8f334bda92a0c
ryanluoo <quick.sword@hotmail.com>
2013-06-12 11:55:27 +0800

    finish Raucous Rocker

--- USACO 3.4/USACO_Raucous Rockers/USACO_Raucous Rockers/rockers_main.cpp
+++ USACO 3.4/USACO_Raucous Rockers/USACO_Raucous Rockers/rockers_main.cpp
@@ -12,40 +12,32 @@ using namespace std;
 int N, T, M;
 char songs[20];
 char s[20];
-int cmp (const void* a, const void* b)
-{
-	return *(char*)a - *(char*)b;
-}
 
-bool Check(int n)
+int Check (int n)
 {
-	int start = 0, end = n - 1;
-	for (int m = 0; m != M; ++m)
+	int fit = 0;
+	int remt = T, m = M;
+	for (int i = 0; i != N; ++i)
 	{
-		if (start > end)
-			break;
-
-		int remt = T;
-		while (remt > 0 && end >= start)
+		if (n & (1 << i))
 		{
-			if (s[end] <= remt)
-				remt -= s[end--];
+			if (m == 0)
+				return fit;
+			
+			if (songs[i] <= remt)
+			{
+				++fit;
+				remt -= songs[i];
+			}
 			else
-				break;
-		}
-
-		while (remt > 0 && end >= start)
-		{
-			if (s[start] <= remt)
-				remt -= s[start++];
-			else
-				break;
+			{
+				--m;				
+				--i;
+				remt = T;
+			}
 		}
 	}
-	if (start <= end)
-		return false;
-	else
-		return true;
+	return fit;
 }
 
 int main()
@@ -61,25 +53,14 @@ int main()
 		songs[i] = t;
 	}
 
-	int maxfit = N;
-	while (maxfit > 0)
+	int maxfit = 0;
+	for (int i = (1 << N) - 1; i > 0; --i)
 	{
-		char flag = 0;
-		for (int i = 0; i + maxfit <= N; ++i)
-		{
-			memcpy(s, songs + i, maxfit);
-			qsort(s, maxfit, sizeof(char), cmp);
-			if (Check(maxfit))
-			{
-				flag = 1;
-				break;
-			}
-		}
-		if (!flag)
-			--maxfit;
-		else
-			break;
+		int fit = Check(i);
+		if (maxfit < fit)
+			maxfit = fit;
 	}
+
 	fout << maxfit << endl;
 
 	fin.close();
