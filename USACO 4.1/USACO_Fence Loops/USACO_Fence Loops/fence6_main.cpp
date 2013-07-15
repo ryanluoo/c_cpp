6427c29696a29a136575d2e44f6905a279aa8a56
ryanluoo <quick.sword@hotmail.com>
2013-07-15 22:11:36 +0800

    Update

--- USACO 4.1/USACO_Fence Loops/USACO_Fence Loops/fence6_main.cpp
+++ USACO 4.1/USACO_Fence Loops/USACO_Fence Loops/fence6_main.cpp
@@ -8,11 +8,129 @@ LANG: C++
 #include <cstring>
 using namespace std;
 
+#define MAXN 100
+
+struct LINE
+{
+	int ID ;
+	int len;
+	int N1, N2;
+	int pid1, pid2;
+	int join1[8];
+	int join2[8];
+};
+
+int N;
+int i, j, k;
+LINE lines[MAXN];
+int dist[MAXN][MAXN] = {0};
+int pred[MAXN][MAXN] = {0};
+
 int main()
 {
 	ifstream fin ("fence6.in" );
 	ofstream fout("fence6.out");
 	
+	fin >> N;
+	for (i = 0; i != N; ++i)
+	{
+		fin >> lines[i].ID >> lines[i].len;
+		fin >> lines[i].N1 >> lines[i].N2;
+		for (j = 0; j != lines[i].N1; ++j)
+			fin >> lines[i].join1[j];
+		for (j = 0; j != lines[i].N2; ++j)
+			fin >> lines[i].join2[j];
+		lines[i].pid1 = lines[i].pid2 = -1;
+	}
+	
+	int pid = 0;
+	for (i = 0; i != N; ++i)
+	{
+		if (lines[i].pid1 == -1)
+		{
+			lines[i].pid1 = pid;
+			++pid;
+			for (j = 0; j != lines[i].N1; ++j)
+			{
+				int conn = lines[i].join1[j] - 1;
+				if (lines[conn].pid1 == -1)
+					lines[conn].pid1 = lines[i].pid1;
+				else
+					lines[conn].pid2 = lines[i].pid1;
+			}
+		}
+	
+		if (lines[i].pid2 == -1)
+		{
+			lines[i].pid2 = pid;
+			++pid;
+			for (j = 0; j != lines[i].N2; ++j)
+			{
+				int conn = lines[i].join2[j] - 1;
+				if (lines[conn].pid1 == -1)
+					lines[conn].pid1 = lines[i].pid2;
+				else
+					lines[conn].pid2 = lines[i].pid2;
+			}
+		}
+	}
+
+	//initial
+	for (i = 0; i != pid; ++i)
+	{
+		for (j = 0; j != pid; ++j)
+		{
+			if (i == j)
+				dist[i][j] = pred[i][j] = 0;
+			else
+				dist[i][j] = pred[i][j] = 0x7fffffff;
+
+		}
+	}
+
+
+	for (i = 0; i != N; ++i)
+	{
+		dist[lines[i].pid1][lines[i].pid2] = lines[i].len;
+		dist[lines[i].pid2][lines[i].pid1] = lines[i].len;
+	}
+
+	for (i = 0; i != pid; ++i)
+	{
+		for (j = 0; j != pid; ++j)
+		{
+			for (k = 0; k != pid; ++k)
+			{
+				if (dist[i][k] != 0x7fffffff && dist[k][j] != 0x7fffffff && (
+				    dist[i][j] == 0x7fffffff || dist[i][j] >= dist[i][k] + dist[k][j]))
+				{
+					if (dist[i][j] > dist[i][k] + dist[k][j])
+						pred[i][j] = dist[i][j];
+					dist[i][j] = dist[i][k] + dist[k][j];
+				}
+				else if (dist[i][k] != 0x7fffffff && dist[k][j] != 0x7fffffff && ((pred[i][j] == 0x7fffffff || dist[i][k] + dist[k][j] < pred[i][j])))
+				{
+					pred[i][j] = dist[i][k] + dist[k][j];
+				}
+			}
+		}
+	}
+	
+	int minloop = 0x7fffffff;
+	for (i = 0; i != pid; ++i)
+	{
+		for (j = 0; j != pid; ++j)
+		{
+			if (pred[i][j] != 0x7fffffff && dist[i][j] != 0x7fffffff && pred[i][j] != dist[i][j])
+			{
+				if (minloop > pred[i][j] + dist[i][j])
+					minloop = pred[i][j] + dist[i][j];
+			}
+		}
+	}
+
+	fout << minloop << endl;
+
 	fin.close();
 	fout.close();
 	return 0;
--- USACO 4.1/USACO_Fence Rails/USACO_Fence Rails/fence8_main.cpp
+++ USACO 4.1/USACO_Fence Rails/USACO_Fence Rails/fence8_main.cpp
@@ -4,15 +4,138 @@ PROG: fence8
 LANG: C++
 */
 
+//#include <fstream>
+//#include <algorithm>
+//#include <set>
+//using namespace std;
+//ifstream fin("fence8.in");
+//ofstream fout("fence8.out");
+//int N,R,A[2048],Cf[60],Now[60],Pre[2048],Max,Sums,Limit,Tot;
+//bool flag;
+//bool Cmp(int a,int b)
+//{
+//	return a>b;
+//}
+//void Initialize()
+//{
+//	int i;
+//	fin>>N;
+//	for(i=1;i<=N;i++)
+//	{
+//		fin>>Cf[i];
+//		Tot+=Cf[i];
+//	}
+//	sort(Cf+1,Cf+N+1);
+//	fin>>R;
+//	for(i=1;i<=R;i++)
+//		fin>>A[i];
+//	sort(A+1,A+R+1);
+//	for(i=1;i<=R;i++)
+//		if(A[i]>Cf[N])
+//			break;
+//	R=i-1;
+//	for(i=1;i<=R;i++)
+//		Pre[i]=Pre[i-1]+A[i];
+//}
+//
+//void DFS(int pos,int Prev)
+//{
+//	int i,st,R=0;
+//	if(Sums>Tot-Pre[Limit])
+//		return;
+//	if(pos>Limit)
+//		flag=true;
+//	if(flag)
+//		return;
+//	st=1;
+//	if(A[Limit-pos+1]==A[Limit-pos+2])
+//		st=Prev;
+//	for(i=st;i<=N;i++)
+//		if(A[Limit-pos+1]<=Cf[i])
+//		{
+//			Cf[i]-=A[Limit-pos+1];
+//			if(Cf[i]<A[1])
+//				Sums+=Cf[i];
+//			DFS(pos+1,i);
+//			if(Cf[i]<A[1])
+//				Sums-=Cf[i];
+//			Cf[i]+=A[Limit-pos+1];
+//		}
+//}
+//
+//int main()
+//{
+//	Initialize();
+//
+//	for(Limit=1;Limit<=R;Limit++)
+//	{
+//		flag=false;
+//		DFS(1,1);
+//		if(!flag)
+//			break;
+//	}
+//	fout<<Limit-1<<endl;
+//	fin.close();
+//	fout.close();
+//	return 0;
+//}
+
+
 #include <fstream>
-#include <cstring>
+#include <cstdlib>
+#include <climits>
+#include <map>
+#include <stack>
 using namespace std;
+
+#define MAXN  50
+#define MAXR  1023
+#define MAXRL 128
+
+int N, R;
+int boards[MAXN], rails[MAXR], sumrails[MAXR], sumboards;
+int i, j;
+stack< pair<int, int> > dfsstack;
+int result = 0;
+
+int cmp (const void *a, const void *b)
+{
+	return *(int*)a - *(int*)b;
+}
 
 int main()
 {
 	ifstream fin ("fence8.in" );
 	ofstream fout("fence8.out");
+
+	fin >> N;
+	sumboards = 0;
+	for (i = 0; i != N; ++i)
+	{
+		fin >> boards[i];
+		sumboards += boards[i];
+	}
+
+	fin >> R;
+	for (i = 0; i != R; ++i)
+		fin >> rails[i];
 	
+	qsort(boards, N, sizeof(boards[0]), cmp);
+	qsort( rails, R, sizeof( rails[0]), cmp);
+
+	//initial
+	sumrails[0] = rails[0];
+	for (i = 1; i != R; ++i)
+	{
+		sumrails[i] = sumrails[i-1] + rails[i];
+		if (rails[i] > boards[N-1] || sumrails[i] > sumboards)
+			break;
+	}
+	R = i;
+	//////////////////////////////////////////////
+	
+
+
 	fin.close();
 	fout.close();
 	return 0;
